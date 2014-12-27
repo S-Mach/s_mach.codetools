19156e060ef49ffe0945436facf448fd92a07294
Lance Gatlin <lance.gatlin@gmail.com>
2014-12-27 14:44:12 -0500

    docs for Result

--- codetools-core/src/main/scala/s_mach/codetools/Result.scala
+++ codetools-core/src/main/scala/s_mach/codetools/Result.scala
@@ -3,80 +3,55 @@ package s_mach.codetools
 import scala.language.higherKinds
 import scala.collection.generic.CanBuildFrom
 
-sealed trait IssueLevel
-object IssueLevel {
-  case object Error extends IssueLevel
-  case object Warning extends IssueLevel
-}
-case class Issue(level: IssueLevel, message: String)
 
+/**
+ * A monad for the result of some computation that may fail with multiple errors
+ * or complete successfully with warnings. The order that warnings and errors
+ * accumulate is preserved.
+ *
+ * @tparam A type of the computation
+ */
 sealed trait Result[+A] {
+  import Result._
+
+  /** @return TRUE if the result is successful */
   def isSuccess : Boolean
+  /** @return TRUE if the result is a failure */
   def isFailure : Boolean
+  /**
+   * @throws NoSuchElementException if the result is a failure
+   * @return the value of the computation */
   def get : A
+  /** @return zero or more issues raised during the computation */
   def zomIssue: List[Issue]
+  /** @return If Result is successful, a new Result with the value returned from
+    *         applying the value to f. If failure, no change */
   def map[B](f: A => B) : Result[B]
+  /** @return If Result is successful, a new Result with the value returned from
+    *         applying the value to f . If failure, no change */
   def flatMap[B](f: A => Result[B]) : Result[B]
+  /** @return If successful, Some(value) otherwise None */
   def toOption: Option[A]
+  /** @return a new Result with the supplied list of issues prepended to the
+    *         list of issues*/
   def prepend(_zomIssue: List[Issue]) : Result[A]
-  def fold[X, AA >: A](isSuccess: Result.Success[AA] => X, isFailure: Result.Failure => X) : X
+  /** @return If successful, the result of applying this to isSuccess. If
+    *         failure, the result fo applying this to isFailure */
+  def fold[X, AA >: A](isSuccess: Success[AA] => X, isFailure: Failure => X) : X
 }
 
 object Result {
-  object Error {
-    def apply(message: String) : Issue = Issue(IssueLevel.Error, message)
-    def unapply(issue: Issue) : Option[String] = issue.level match {
-      case IssueLevel.Error => Some(issue.message)
-      case _ => None
-    }
-  }
-  object Warning {
-    def apply(message: String) : Issue = Issue(IssueLevel.Warning, message)
-    def unapply(issue: Issue) : Option[String] = issue.level match {
-      case IssueLevel.Warning => Some(issue.message)
-      case _ => None
-    }
-  }
-
-  def sequence[A,M[AA] <: Traversable[AA]](
-    m: M[Result[A]]
-  )(implicit
-    cbf:CanBuildFrom[Nothing,A,M[A]]
-  ) : Result[M[A]] = {
-    val issueBuilder = List.newBuilder[Issue]
-    m.foreach(r => issueBuilder ++= r.zomIssue)
-    val zomIssue = issueBuilder.result()
-    if(m.forall(_.isSuccess)) {
-      val builder = cbf()
-      m.foreach(r => builder += r.get)
-      Success(builder.result(),zomIssue)
-    } else {
-      Failure(zomIssue)
-    }
-  }
-
-  def applicative[A,B,ZZ](ra: Result[A], rb: Result[B])(f: (A,B) => Result[ZZ]) : Result[ZZ] = {
-    val zomIssue = ra.zomIssue ::: rb.zomIssue
-    if(ra.isSuccess && rb.isSuccess) {
-      f(ra.get,rb.get).prepend(zomIssue)
-    } else {
-      Failure(zomIssue)
-    }
-  }
-  def applicative[A,B,C,ZZ](ra: Result[A], rb: Result[B],rc: Result[C])(f: (A,B,C) => Result[ZZ]) : Result[ZZ] = {
-    val zomIssue = ra.zomIssue ::: rb.zomIssue ::: rc.zomIssue
-    if(ra.isSuccess && rb.isSuccess && rc.isSuccess) {
-      f(ra.get,rb.get,rc.get).prepend(zomIssue)
-    } else {
-      Failure(zomIssue)
-    }
-  }
-  def applicative[A,B,C,D,ZZ](ra: Result[A], rb: Result[B],rc: Result[C],rd: Result[D])(f: (A,B,C,D) => Result[ZZ]) : Result[ZZ] = {
-    val zomIssue = ra.zomIssue ::: rb.zomIssue ::: rc.zomIssue ::: rd.zomIssue
-    if(ra.isSuccess && rb.isSuccess && rc.isSuccess) {
-      f(ra.get,rb.get,rc.get,rd.get).prepend(zomIssue)
-    } else {
-      Failure(zomIssue)
+  /** @return a success result with possible warnings */
+  def apply[A](value: A, warning: String*) : Success[A] =
+    Success(value, warning.map(Warning.apply).toList)
+  /** @return a failing result with an error */
+  def error[A](msg: String) : Result[A] = Failure(Error(msg) :: Nil)
+  /** @return a failing result with errors */
+  def errors[A](msg: String) : Result[A] = Failure(Error(msg) :: Nil)
+  def unapply[A](r: Result[A]) : Option[(Option[A],List[Issue])] = Some{
+    r match {
+      case Success(value, oomIssue) => (Some(value), oomIssue)
+      case Failure(oomIssue) => (None, oomIssue)
     }
   }
 
@@ -122,14 +97,54 @@ object Result {
     override def fold[X, AA >: A](isSuccess: Success[AA] => X, isFailure: Failure => X): X = isSuccess(this)
   }
 
-  def error[A](msg: String) : Result[A] = Failure(Error(msg) :: Nil)
-  def errors[A](msg: String) : Result[A] = Failure(Error(msg) :: Nil)
-  def apply[A](value: A, warning: String*) : Success[A] = Success(value, warning.map(Warning.apply).toList)
-  def unapply[A](r: Result[A]) : Option[(Option[A],List[Issue])] = Some{
-    r match {
-      case Success(value, oomIssue) => (Some(value), oomIssue)
-      case Failure(oomIssue) => (None, oomIssue)
+  sealed trait Issue {
+    def message: String
+  }
+  case class Error(message: String) extends Issue
+  case class Warning(message: String) extends Issue
+
+  def sequence[A,M[AA] <: Traversable[AA]](
+    m: M[Result[A]]
+  )(implicit
+    cbf:CanBuildFrom[Nothing,A,M[A]]
+  ) : Result[M[A]] = {
+    val issueBuilder = List.newBuilder[Issue]
+    m.foreach(r => issueBuilder ++= r.zomIssue)
+    val zomIssue = issueBuilder.result()
+    if(m.forall(_.isSuccess)) {
+      val builder = cbf()
+      m.foreach(r => builder += r.get)
+      Success(builder.result(),zomIssue)
+    } else {
+      Failure(zomIssue)
     }
   }
+
+  def applicative[A,B,ZZ](ra: Result[A], rb: Result[B])(f: (A,B) => Result[ZZ]) : Result[ZZ] = {
+    val zomIssue = ra.zomIssue ::: rb.zomIssue
+    if(ra.isSuccess && rb.isSuccess) {
+      f(ra.get,rb.get).prepend(zomIssue)
+    } else {
+      Failure(zomIssue)
+    }
+  }
+  def applicative[A,B,C,ZZ](ra: Result[A], rb: Result[B],rc: Result[C])(f: (A,B,C) => Result[ZZ]) : Result[ZZ] = {
+    val zomIssue = ra.zomIssue ::: rb.zomIssue ::: rc.zomIssue
+    if(ra.isSuccess && rb.isSuccess && rc.isSuccess) {
+      f(ra.get,rb.get,rc.get).prepend(zomIssue)
+    } else {
+      Failure(zomIssue)
+    }
+  }
+  def applicative[A,B,C,D,ZZ](ra: Result[A], rb: Result[B],rc: Result[C],rd: Result[D])(f: (A,B,C,D) => Result[ZZ]) : Result[ZZ] = {
+    val zomIssue = ra.zomIssue ::: rb.zomIssue ::: rc.zomIssue ::: rd.zomIssue
+    if(ra.isSuccess && rb.isSuccess && rc.isSuccess) {
+      f(ra.get,rb.get,rc.get,rd.get).prepend(zomIssue)
+    } else {
+      Failure(zomIssue)
+    }
+  }
+
+
 
 }

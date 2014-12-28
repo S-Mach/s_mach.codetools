7c84f65435c2932f50bfa5c9964c91a782f22ddb
Lance Gatlin <lance.gatlin@gmail.com>
2014-12-27 23:01:17 -0500

    added headers [skip ci]

--- src/test/scala/s_mach/codetools/ExampleCodeTest.scala
+++ src/test/scala/s_mach/codetools/ExampleCodeTest.scala
@@ -1,3 +1,21 @@
+/*
+                    ,i::,
+               :;;;;;;;
+              ;:,,::;.
+            1ft1;::;1tL
+              t1;::;1,
+               :;::;               _____       __  ___              __
+          fCLff ;:: tfLLC         / ___/      /  |/  /____ _ _____ / /_
+         CLft11 :,, i1tffLi       \__ \ ____ / /|_/ // __ `// ___// __ \
+         1t1i   .;;   .1tf       ___/ //___// /  / // /_/ // /__ / / / /
+       CLt1i    :,:    .1tfL.   /____/     /_/  /_/ \__,_/ \___//_/ /_/
+       Lft1,:;:       , 1tfL:
+       ;it1i ,,,:::;;;::1tti      s_mach.codetools
+         .t1i .,::;;; ;1tt        Copyright (c) 2014 S-Mach, Inc.
+         Lft11ii;::;ii1tfL:       Author: lance.gatlin@gmail.com
+          .L1 1tt1ttt,,Li
+            ...1LLLL...
+*/
 package s_mach.codetools
 
 import org.scalatest.{Matchers, FlatSpec}
@@ -58,6 +76,7 @@ class ExampleCodeTest extends FlatSpec with Matchers {
     a1.printApply should equal(
       """Actor(name=Name(firstName="Gary",middleName=Some("Freakn"),lastName="Oldman"),age=56,movies=Set(Movie(name="The Professional",year=1994),Movie(name="The Fifth Element",year=1997)))"""
     )
+    // TODO: eval code instead of copy paste here
     val alt1 = Actor(name=Name(firstName="Gary",middleName=Some("Freakn"),lastName="Oldman"),age=56,movies=Set(Movie(name="The Professional",year=1994),Movie(name="The Fifth Element",year=1997)))
     alt1 should equal(a1)
   }
